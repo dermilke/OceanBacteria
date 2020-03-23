3780046e4ff99e7e3c4952eefb77ccfadeaee056
Felix Milke <dermilke@googlemail.com>
2020-03-23 12:28:17 +0100

    Adding file for local adjustments.

---
+++ EditMe.R
@@ -0,0 +1,14 @@
+### Edit this file to adjust to local settings ###
+
+# BLAST-Settings:
+# Make sure that the folder of the queryFile is writable!
+blastDB <- "~/PhD/Statistics/Shiny_Test/Test/Data/BlastDB/myDB"
+queryFile <- "~/PhD/Statistics/Shiny_Test/Test/Data/query.fa"
+blastCommand <- "/usr/local/ncbi/blast/bin/blastn"
+
+# Data Storage:
+Atlantic_Count_Data <- "~/PhD/Statistics/Shiny_Test/Test/Atlantic_Count_Data.csv"
+Atlantic_Meta_Data <- "~/PhD/Statistics/Shiny_Test/Test/Atlantic_Meta_Data.csv"
+
+Pacific_Count_Data <- "~/PhD/Statistics/Shiny_Test/Test/Pacific_Count_Data.csv"
+Pacific_Meta_Data <- "~/PhD/Statistics/Shiny_Test/Test/Pacific_Meta_Data.csv"
