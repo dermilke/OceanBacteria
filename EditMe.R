5be916fd6602815f7b9bccff05883ebd711fd4b9
Felix Milke <dermilke@googlemail.com>
2020-03-23 12:53:53 +0100

    Simplify EditMe

--- EditMe.R
+++ EditMe.R
@@ -2,13 +2,13 @@
 
 # BLAST-Settings:
 # Make sure that the folder of the queryFile is writable!
-blastDB <- "~/PhD/Statistics/Shiny_Test/Test/Data/BlastDB/myDB"
-queryFile <- "~/PhD/Statistics/Shiny_Test/Test/Data/query.fa"
+blastDB <- "/Data/BlastDB/myDB"
+queryFile <- "/Data/query.fa"
 blastCommand <- "/usr/local/ncbi/blast/bin/blastn"
 
 # Data Storage:
-Atlantic_Count_Data <- "~/PhD/Statistics/Shiny_Test/Test/Atlantic_Count_Data.csv"
-Atlantic_Meta_Data <- "~/PhD/Statistics/Shiny_Test/Test/Atlantic_Meta_Data.csv"
+Atlantic_Count_Data <- "/Data/Atlantic_Count_Data.csv"
+Atlantic_Meta_Data <- "/Data/Atlantic_Meta_Data.csv"
 
-Pacific_Count_Data <- "~/PhD/Statistics/Shiny_Test/Test/Pacific_Count_Data.csv"
-Pacific_Meta_Data <- "~/PhD/Statistics/Shiny_Test/Test/Pacific_Meta_Data.csv"
+Pacific_Count_Data <- "/Data/Pacific_Count_Data.csv"
+Pacific_Meta_Data <- "/Data/Pacific_Meta_Data.csv"
